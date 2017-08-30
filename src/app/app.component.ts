ed35719499a28f3dba3be2d9c5ecbb1e80e34d05
Lucas Thompson <dev@lucas.im>
2017-08-30 17:12:37 +0100

    Setup project with basic drag and drop boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ng2FileDropAcceptedFile} from 'ng2-file-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  private dragFileAccepted(acceptedFile: Ng2FileDropAcceptedFile) {
+    console.warn(acceptedFile);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { Ng2FileDropModule }  from 'ng2-file-drop';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2FileDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
