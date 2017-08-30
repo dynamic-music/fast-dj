fc5e169cdb99bcedfc85204238eb9472348c30fe
Florian Thalmann <florianthalmann@bluewin.ch>
2017-08-30 18:38:05 +0100

    basic playback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Ng2FileDropAcceptedFile} from 'ng2-file-drop';
+import { DymoGenerator, ExpressionGenerator, DymoManager, uris } from 'dymo-core';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,33 @@ import {Ng2FileDropAcceptedFile} from 'ng2-file-drop';
 })
 export class AppComponent {
   title = 'app';
+  private dymoGen: DymoGenerator;
+  private exprGen: ExpressionGenerator;
+  private manager: DymoManager;
+  private isPlaying = false;
+
+  constructor() {
+    this.dymoGen = new DymoGenerator();
+    this.exprGen = new ExpressionGenerator(this.dymoGen.getManager().getStore());
+    this.manager = this.dymoGen.getManager();
+  }
 
   private dragFileAccepted(acceptedFile: Ng2FileDropAcceptedFile) {
-    console.warn(acceptedFile);
+    const url = URL.createObjectURL(acceptedFile.file);
+    console.warn(url);
+    const newDymoUri = this.dymoGen.addDymo(null, url);
+    let rampUri = this.dymoGen.addControl(null, uris.RANDOM);
+    this.exprGen.addConstraint(newDymoUri, `
+      ∀ d in ["`+newDymoUri+`"]
+      => ∀ c in ["`+rampUri+`"]
+      => Amplitude(d) == c
+    `);
+    this.manager.reloadFromStore()
+      .then(() => {
+        if (!this.isPlaying) {
+          this.manager.startPlaying();
+          this.isPlaying = true;
+        }
+      });
   }
 }
