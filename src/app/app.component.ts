242c2df8f6d1b62f8b614a61ca0de01b93504513
Florian Thalmann <florianthalmann@bluewin.ch>
2017-09-04 00:04:46 +0100

    added simple mixing example and cleaned up a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {Ng2FileDropAcceptedFile} from 'ng2-file-drop';
-import { DymoGenerator, ExpressionGenerator, DymoManager, uris } from 'dymo-core';
+import { Ng2FileDropAcceptedFile } from 'ng2-file-drop';
+import { DymoGenerator, DymoTemplates, MixGenerator, DymoManager, GlobalVars, uris } from 'dymo-core';
 import {
   FeatureExtractionService
 } from './services/feature-extraction/feature-extraction.service';
@@ -13,48 +13,36 @@ import {
 export class AppComponent {
   title = 'app';
   private dymoGen: DymoGenerator;
-  private exprGen: ExpressionGenerator;
+  private mixGen: MixGenerator;
   private manager: DymoManager;
+  private isPlaying = false;
 
   constructor(private extractionService: FeatureExtractionService) {
-    this.dymoGen = new DymoGenerator('https://semantic-player.github.io/dymo-core/ontologies/');
-    this.exprGen = new ExpressionGenerator(this.dymoGen.getManager().getStore());
-    this.manager = this.dymoGen.getManager();
+    GlobalVars.LOGGING_ON = true;
+    this.manager = new DymoManager();
+    this.manager.init('https://semantic-player.github.io/dymo-core/ontologies/')
+      .then(() => {
+        let store = this.manager.getStore();
+        this.dymoGen = new DymoGenerator(store);
+        this.mixGen = new MixGenerator(this.dymoGen);
+      })
   }
 
   private dragFileAccepted(acceptedFile: Ng2FileDropAcceptedFile) {
     const url = URL.createObjectURL(acceptedFile.file);
-    console.warn(url);
-    console.time('Audio');
-    this.manager.getAudioBank().getBuffer(url).then(buffer => {
-      console.timeEnd('Audio');
-      console.warn(buffer);
-      console.time('Beats');
-      return this.extractionService.extractBeats(buffer);
-    }).then(beats => {
-      console.timeEnd('Beats');
-      console.warn(beats);
-    });
-
-    
-
-    const newDymoUri = this.dymoGen.addDymo(null, url);
-
-    //scheduleGen.generateTransition(uris.IMMEDIATE, newDymoUri)
-    //scheduleGen.generateTransition(uris.CROSSFADE, newDymoUri)
-    //scheduleGen.generateTransition(uris.BEATMATCH, newDymoUri)
-    //IF NOTHING PLAYING YET, SIMPLY ADD, ELSE MAKE TRANSITION
-
-    const rampUri = this.dymoGen.addControl(null, uris.BROWNIAN);
-    const constraintUri = this.exprGen.addConstraint(newDymoUri, `
-      ∀ d in ["`+newDymoUri+`"]
-      => ∀ c in ["`+rampUri+`"]
-      => Amplitude(d) == c
-    `);
-    //this.scheduleGen
-    this.manager.loadFromStore(newDymoUri, rampUri, constraintUri)
-      .then(() => {
-        this.manager.startPlaying();
-      });
+    this.manager.getAudioBank().loadBuffer(url)
+      .then(buffer => this.extractionService.extractBeats(buffer))
+      .then(beats => DymoTemplates.createAnnotatedBarAndBeatDymo2(this.dymoGen, url, beats))
+      .then(newDymo => this.manager.loadFromStore(newDymo)
+        .then(() => this.mixGen.transitionToSong(newDymo, 1)))
+      .then(mixDymo => this.keepOnPlaying(mixDymo));
   }
+
+  private keepOnPlaying(dymoUri: string) {
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      this.manager.startPlayingUri(dymoUri);
+    }
+  }
+
 }
